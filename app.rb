d5879299637225014c490a4c741ae8237cec78d8
github username <github email address>
2017-10-30 22:44:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,53 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number]
+    "#{@number.to_i ** 2}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+
+    full_string = ""
+    @number.to_i.times do
+      full_string += @phrase + "/n"
+    end
+    "#{full_string}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    sentence = @word1 + " " + @word2 + " " + @word3 + " " + @word4 + " " + @word5 + "."
+    "#{sentence}"
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    case @operation
+    when "add"
+      out = @number1 + @number2
+    when "subtract"
+      out = @number1 - @number2
+    when "multiply"
+      out = @number1 * @number2
+    when "divide"
+      out = @number1 / @number2
+    end
+    "#{out}"
+  end
 
 end
